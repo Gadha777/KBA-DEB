a91139882bf961e6f23ba95bc2d6b077bebe341f
Gadha777 <gadhapushpan2003@gmail.com>
2024-10-03 14:24:37 +0530

    commit

---
+++ JAVASCRIPT/js/function/day3/script.js
@@ -0,0 +1,16 @@
+// type conversion 
+console.log("20"+5);//output 205 because 20 is a string concatination iss working
+console.log("20"-5);// here the output is 15 because 20 is taken as 20 because "-"so it the 2 value as interger
+console.log("20"*5);//100
+console.log("20"/5);//4
+console.log(true+1);//2
+console.log(Number("42"));//42
+console.log(Number("hello"));//NaN not a number
+console.log(typeof String(123));//"123"
+console.log(String(true));//42
+console.log(Boolean(0));//false
+console.log(Boolean(''));//false
+console.log(Boolean('hello'));//true
+console.log(parseInt("15.78"));//15
+console.log(parseFloat("15.78"));//15.78
+
